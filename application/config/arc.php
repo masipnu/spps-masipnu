db93b855e42ccc743ae9cb950fb7d63decb4f48b
Ipnu Masyaid <masipnu1@gmail.com>
2025-06-27 13:55:50 +0700

    mod commit

---
+++ application/config/arc.php
@@ -0,0 +1,45 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Media
+|--------------------------------------------------------------------------
+| Konfigurasi untuk lokasi media
+| 
+|
+*/
+$config['media_url'] = '-';
+
+/*
+|--------------------------------------------------------------------------
+| Setting
+|--------------------------------------------------------------------------
+|
+| 
+|
+*/
+// $config['app_name'] = 'SPPS-APP';
+$config['app_name'] = 'SPPS-APP';
+// $config['version'] = 'V.3.0';
+$config['version'] = 'v3.0';
+// $config['created'] = 'Copyright © 2017-'.date('Y').' <a target="_blank" href="#">Digitalkoding.com</a>. All rights reserved.';
+$config['created'] = 'Mod with ❤️ by <a target="_blank" href="#">Masipnu</a> @ '.date('Y').'.';
+// 
+
+/*
+|--------------------------------------------------masipnu.my.id-------
+| Email
+|--------------------------------------------------------------------------
+|
+| Configuration email to send email notification
+|
+*/
+$config['email'] = TRUE;
+
+/*
+|--------------------------------------------------------------------------
+| Email disclaimer
+|--------------------------------------------------------------------------
+|
+*/
+$config['email_disclaimer'] = "\n\n<em>Email ini dikirim secara otomatis, mohon untuk tidak membalas email ini. Jika mengalami kendala teknis maka dapat menghubungi <a href=\"mailto:\">email@gmail.com</a></em>";
